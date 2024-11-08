e240b2fc707f6f251e2b52c3039c847c2c10ae52
chris_jpark <chrisj.park@mail.utoronto.ca>
2024-11-08 13:54:32 -0500

    fix db not loading on visiting diff page

--- frontend/src/pages/ListingsPage.jsx
+++ frontend/src/pages/ListingsPage.jsx
@@ -21,7 +21,7 @@ function ListingsGrid({listings, setListings}) {
 
   const fetchFavorites = async () => {
     try {
-      const userId = JSON.parse(localStorage.getItem('user')).id;
+      const userId = 1;
       const response = await fetch(`http://localhost:5001/api/favorites/${userId}`);
       const data = await response.json();
 
