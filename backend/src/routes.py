347ee86640e7fda09994055764d3b2d3b81802ce
chris_jpark <chrisj.park@mail.utoronto.ca>
2024-11-08 12:12:59 -0500

    update db location in backend

--- backend/src/routes.py
+++ backend/src/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from app.models import db, User, Item, ItemImage, Favorite
+from src.models import db, User, Item, ItemImage, Favorite
 import base64
 
 bp = Blueprint('main', __name__)
